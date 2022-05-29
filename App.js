cbc9ca0a17cbd573d929a430ef8844c4e7aaee03
Vishal Gupta <90566964+guptav100@users.noreply.github.com>
2022-05-29 11:54:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+
+import NavBar from "./components/Navbar";
+import Banner from "./components/Banner";
+import MovieList from "./components/MovieList";
+import Favourites from "./components/Favourites";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner /> 
+              <MovieList />
+            </>
+          }
+        />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
